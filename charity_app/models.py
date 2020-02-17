1dbe748e5a1dd8df835d172752ef91e0053a40b2
Maksymilian Czubak <maksymilian.czubak@gmail.com>
2020-02-17 21:20:28 +0100

    First Commit

---
+++ charity_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+TYPE = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna'),
+)
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE, default=1)
+    category = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete = models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=256)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, blank=True, default=None, on_delete = models.CASCADE)
