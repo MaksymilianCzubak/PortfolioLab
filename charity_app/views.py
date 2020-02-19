cd3cadf700c75248402b5a6f76216f886fdbd567
Maksymilian Czubak <maksymilian.czubak@gmail.com>
2020-02-19 21:49:17 +0100

    Add corrections day 3

--- charity_app/views.py
+++ charity_app/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Donation, Institution
 # Create your views here.
 
 class LandingPage(View):
     def get(self, request):
-        ctx = {}
+        sum_of_sucks = Donation.objects.all().count()
+        institutions = Institution.objects.all().count()
+        ctx = {'sum_of_sucks': sum_of_sucks,
+               'sum_of_institutions': institutions,
+               }
         return render(request, "index.html", ctx)
 
 class AddDonation(View):
     def get(self, request):
         ctx = {}
-        return render(request, "index.html", ctx)
+        return render(request, "form.html", ctx)
 
 class Login(View):
     def get(self, request):
